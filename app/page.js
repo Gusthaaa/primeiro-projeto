558d47edd6e2a843d405eb50f7adbc9de329274d
Gustavo Hatanaka <Gustavohatanaka@edu.unifil.br>
2023-09-19 14:28:25 -0300

    ESLint changes

--- app/page.js
+++ app/page.js
@@ -16,7 +16,7 @@ const homePage = () => {
                 <h1 className="font-bold italic text-white mt-7">
                   Hello! My name is Gustavo Toyoshi Hatanaka.
                 </h1>
-                <h2 className='text-white italic'>Born in Londrina in Brazil. I'm an undergraduate Software Engineer with a passion for solving problems with code. I started my journey into the world of software develpment a year ago, and since then i've been constantly learning and improving my skills.</h2>
+                <h2 className="text-white italic">Born in Londrina in Brazil. I'm an undergraduate Software Engineer with a passion for solving problems with code. I started my journey into the world of software develpment a year ago, and since then i've been constantly learning and improving my skills.</h2>
               </div>
         </section>
         <section className="h-40">
@@ -24,7 +24,7 @@ const homePage = () => {
         </section>
         <section className="flex flex-row bg-slate-500 h-1/4">
         <div className="flex flex-col h-24 mt-8 ml-3">
-                <h1 className='text-white italic mt-10 p-2'>
+                <h1 className="text-white italic mt-10 p-2">
                   When I'm not coding, I enjoy spendind time with my family and friend, watching movies, and playing video games. I'm a lifelong learner, and I'm always looking for new books, tutorial, and courses to help me improve my skills and knowlegde. If you're interested in learning more about my skills and experience, feel free to take a look at my CV. I'd love to hear from you and discuss potential opportunities to collaborate on projects or learn from each other.
                 </h1>
               </div>
