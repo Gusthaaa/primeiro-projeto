c85bed40424e2374f86a986eb093114547751e23
Gustavo Hatanaka <Gustavohatanaka@edu.unifil.br>
2023-09-19 14:58:32 -0300

    project page changes

--- app/my-projects/page.js
+++ app/my-projects/page.js
@@ -4,12 +4,12 @@ const Projects = () =>{
     return (
         <div className="h-screen bg-slate-700 text-white mt-24">
             <h1 className="font-bold italic text-center text-xl">My Projects</h1>
-            <section className="flex h-24 mt-8  bg-slate-500 w-screen h-1/3">
+            <section className="flex h-24 mt-8  bg-slate-500 w-screen h-1/3" style={{height: "300px"}}>
                 <Image 
-                className="ml-3 mt-2 mb-2 mr-2 rounded-lg" 
+                className="rounded-lg  max-h-full max-w-full mt-3 ml-5 mb-3 mr-5" 
                 src="/PrimeiroProjeto.png"
-                width={500}
-                height={400}
+                width={400}
+                height={100}
                 alt="Bookstore project"/>
                 <h1 className="aling-middle mt-32 font-bold italic">This is my first project. It's a simulation of a Bookstore, to do this project I used HTML, CSS, JavaScript.</h1>
             </section>
